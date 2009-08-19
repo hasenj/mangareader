30893977631fbdcd014d460680cd1bf6a897aab6
hasen j <hasan.aljudy@gmail.com>
2009-08-19 14:39:51 -0600

    fetching is now working it seems

--- dirlist.py
+++ dirlist.py
@@ -7,6 +7,8 @@
 """
 import os
 
+# ------ some primitives ----------------
+
 def stack_top(stack):
     """Get the top (last item) of the stack """
     return stack[-1]
@@ -20,35 +22,72 @@ class DirEntry(object):
         self.root = root
         self.name = name
         self._isdir = None
-    def path(self):
-        return os.path.join(self.root, self.name)
+        self.path = os.path.join(self.root, self.name)
     def isdir(self):
         if self._isdir is None:
-            self._isdir = os.path.isdir(self.path())
+            self._isdir = os.path.isdir(self.path)
         return self._isdir
     def __repr__(self):
         dir = "dir" if self.isdir() else "file"
         return "[%s] %s" % (self.name, dir) 
 
+def root_path(root):
+    return os.path.realpath(root)
 
 def dir_entry_list(root):
-    root = os.path.realpath(root)
-    return [DirEntry(root, name) for name in os.listdir(root)]
+    root = root_path(root)
+    return [DirEntry(root, name) for name in sorted(os.listdir(root))]
 
-class DirList(object):
+class List(object):
     def __init__(self, root, parent=None):
         """
-            parent is the listring of the parent directory, if any
+            parent is the list of the parent directory, if any
         """
-        root = os.path.realpath(root)
+        root = root_path(root)
         self.root = root
         self.parent = parent
         self.list = dir_entry_list(self.root)
+        self.cursor = 0
+
+    def move_cursor(self, direction=1):
+        if direction not in (1,-1):
+            raise Error("bad direction")
+        self.cursor += direction
+
+    def item(self):
+        return self.list[self.cursor]
+
+    def inbound(self):
+        return self.cursor >= 0 and self.cursor < len(self.list)
 
     def __repr__(self):
         return "\n".join(
                 ["[%s] ---------" % self.root] +
                 ["     %s" % entry for entry in self.list])
+
+FORWARD, BACKWARD = 1, -1
+
+class Fetcher(object):
+    def __init__(self, root, direction=FORWARD):
+        self.root = root_path(root)
+        self.list = List(self.root)
+        self.direction = direction
+
+    def next(self):
+        if not self.list.inbound():
+            if not self.list.parent:
+                return None
+            self.list = self.list.parent
+            return self.next() # try again
+        if self.list.item().isdir():
+            newlist = List(self.list.item().path, self.list)
+            self.list.move_cursor(self.direction)
+            self.list = newlist
+            return self.next() # try again
+        # happy case
+        ret = self.list.item().path
+        self.list.move_cursor(self.direction)
+        return ret
 
 
 
