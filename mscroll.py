ff77079146d3328a59d06fbe469f010a17fc4426
hasen j <hasan.aljudy@gmail.com>
2009-11-13 03:25:21 -0700

    Images loaded in a background thread, in order!

--- mscroll.py
+++ mscroll.py
@@ -5,11 +5,56 @@
     Manager for scrolling manga. The idea is that the scroller doesn't see the entire directory tree,
     only a small window of it, and has to constantly acquire (fetch) next/previous pages (images) as 
     the user scrolls down or up
+
+    fetch.py handles the fetching process.
+
+    fstrip.py is a static film strip mechanism: you have a bunch of images, you draw them 
+        vertically on top of each other, starting at some vertical offset.
+
+    mscroller.py uses the fetcher and the film strip to dynamically fetch and move the vertical
+        list of images as the user scrolls up/down
 """    
 
 import fstrip
 import fetch
 from PyQt4 import QtGui, QtCore
+
+import thread, time
+class ThreadQueue(object):
+    """A thread runner that keeps running various things"""
+    def __init__(self, name):
+        self.name = name
+        self.list = []
+        self.list_lock = thread.allocate_lock()
+        self.thread = thread.start_new_thread(self.run, ())
+    def push(self, target):
+        with self.list_lock:
+            self.list.append(target)
+    def run(self):
+        """
+            This function runs in its own thread!
+        """
+        while True:
+            while len(self.list) == 0:
+                time.sleep(0.1) # wait and check back in a while
+            with self.list_lock:
+                target = self.list.pop(0)
+            target()
+
+threads = {}
+def queue_thread_target(target, name):
+    """
+        Put a function in the queue of a named thread.
+        Right now we only use the image_loader thread, 
+        but it sounds useful to generalize it a bit since it doesn't cost much at all
+
+        @param target: function to run inside thread
+        @param name: the name of the thread 
+    """
+    if not threads.has_key(name):
+        threads[name] = ThreadQueue(name)
+    threads[name].push(target)
+    
 
 class Page(object):
     def __init__(self, path):
@@ -18,15 +63,31 @@ class Page(object):
         self.frame = None
         self.scaled = {}
     def load(self):
-        self.frame = fstrip.create_frame(self.path)
-        self.loaded = True
+        """
+            Load page in the background, if not already loaded.
+
+            This function basically puts the loader function in a background thread.
+
+            But it's actually not just a thread, it puts the loader in a queue, 
+            among with other loader functions that are waiting to be loaded.
+
+            We do this so that images are loaded in order, not randomly (or depending on which is faster to load/smaller in size)
+
+            We'll know when the loader is done because it sets `self.loaded = True`
+        """
+        if self.loaded: return
+        def loader_thread_runner():
+            self.frame = fstrip.create_frame(self.path)
+            self.loaded = True
+        # load image in back ground, keep the order!
+        queue_thread_target(loader_thread_runner, "image_loader")
     def height(self):
         if not self.loaded:
-            self.load()
+            return 10
         return self.frame.rect().height()
     def get_frame(self, width=None):
         if not self.loaded:
-            self.load()
+            return None
         if width is None:
             return self.frame
         if not width in self.scaled:
@@ -68,7 +129,7 @@ class MangaScroller(object):
             self.fetch_more(fetch.BACKWARD)
             # this changes current index, so re-calculate it
             current_index = self.pages.index(self.cursor_page)
-        if self.cursor_pixel > self.cursor_page.height():
+        if self.cursor_page.loaded and self.cursor_pixel > self.cursor_page.height():
             # move to next page
             next_index = current_index + 1
             if len(self.pages) > next_index:
@@ -115,8 +176,9 @@ class FetchError(Exception): pass
 def paint_scroller(painter, scroller, count=3):
     index = scroller.cursor_index()
     pages = scroller.pages[index:index+count]
-    # width = painter.viewport().width() * 0.7 # XXX unstable
-    frames = [p.get_frame() for p in pages]
+    for page in pages:
+        page.load() # load page in background, if not already loaded
+    frames = [p.get_frame() for p in pages if p.loaded]
     y = -scroller.cursor_pixel
     fstrip.paint_frames(painter, frames, y)
 
