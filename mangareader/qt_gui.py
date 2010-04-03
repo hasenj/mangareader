5247f38919ed676fe7e796470fe626c7563b3221
hasen j <hasan.aljudy@gmail.com>
2010-04-02 23:32:20 -0600

    separated manga frame from main window

--- mangareader/qt_gui.py
+++ mangareader/qt_gui.py
@@ -15,71 +15,52 @@ from PyQt4 import QtGui, QtCore
 # Project imports
 from mangareader import fstrip, mscroll
 
+# widget imports
+from widgets.manga_frame import MangaFrame
+
 # XXX This should go away, right after we implement remembering manga location
 if os.name == 'posix':
     test_path = "/home/hasenj/manga/sample/" # TEMP
 elif os.name == 'nt':
     test_path = "C:\manga\sample\\" # TEMP
 
+# constants    
+g_step = 30
+g_big_step = 600
+
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, startdir):
         QtGui.QMainWindow.__init__(self, None)
         self.current_manga_path = startdir
-        self.scroller = mscroll.MangaScroller(startdir)
-        self.step = 100
-        self.big_step = 600
-        self.update_notice = False
-        self.last_pages_count = 0 # we use this to know to re-render when new pages are loaded!
-
-        open = QtGui.QAction(QtGui.QIcon("art/open.png"), "Open Manga", self)
-        open.setShortcut('Ctrl+O')
-        open.setStatusTip('Choose a manga to read')
-        self.connect(open, QtCore.SIGNAL('triggered()'), self.choose_manga)
-
-        self.timer = QtCore.QTimer()
-        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.timerEvent)
-        self.timer.start(80) # number is msec
-
-        # toolbar = self.addToolBar('Manga')
-        # toolbar.addAction(open)
-        # toolbar.hide()
-
-    def change_manga(self, path):
-        # TODO remember where we were last time we were reading this mange
-        # and restore to the same place
-        self.current_manga_path = path
-        self.scroller = mscroll.MangaScroller(path)
-        self.repaint()
+        self.manga_frame = MangaFrame(startdir)
+        self.setCentralWidget(self.manga_frame)
 
     def choose_manga(self):
         dir = QtGui.QFileDialog.getExistingDirectory(self, "Choose Manga", os.path.join(self.current_manga_path, '..'))
         if not dir: return
-        self.change_manga(unicode(dir))
+        path = unicode(dir)
+        self.current_manga_path = path
+        self.manga_frame.change_manga(path)
 
     def choose_chapter(self):
         dir = QtGui.QFileDialog.getExistingDirectory(self, "Choose Chapter", self.current_manga_path)
         if not dir: return
-        self.scroller.change_chapter(unicode(dir))
-
-    def scrollDown(self, step):
-        self.scroller.scroll_down(step)
-    
-    def scrollUp(self, step):
-        self.scroller.scroll_up(step)
+        path = unicode(dir)
+        self.manga_frame.change_chapter(path)
 
     def keyPressEvent(self, event):
         key = event.text()
         qt_key = event.key()
         if key == 'j' or qt_key == QtCore.Qt.Key_Down:
-            self.scrollDown(self.step)
+            self.manga_frame.scrollDown(g_step)
         if key == 'k' or qt_key == QtCore.Qt.Key_Up:
-            self.scrollUp(self.step)
+            self.manga_frame.scrollUp(g_step)
         if key == 'J' or qt_key == QtCore.Qt.Key_PageDown:
-            self.scrollDown(self.big_step)
+            self.manga_frame.scrollDown(g_big_step)
         if key == 'K' or qt_key == QtCore.Qt.Key_PageUp:
-            self.scrollUp(self.big_step)
+            self.manga_frame.scrollUp(g_big_step)
         if key == ' ':
-            self.scrollDown(self.big_step)
+            self.manga_frame.scrollDown(g_big_step)
         if key == 'q' or qt_key == QtCore.Qt.Key_Escape:
             QtGui.QApplication.instance().quit()
         if key == 'o':
@@ -88,22 +69,11 @@ class MainWindow(QtGui.QMainWindow):
             self.choose_chapter()
         if key == ':':
             print "cmdbar TODO"
-        self.repaint()
+        self.manga_frame.repaint()
 
     def wheelEvent(self, event):
-        self.scroller.move_cursor(-event.delta())
-        self.repaint()
-
-    def paintEvent(self, event):
-        painter = QtGui.QPainter()
-        painter.begin(self)
-        try: self.last_pages_count = mscroll.paint_scroller(painter, self.scroller)
-        except: print "error in painting"
-        painter.end()
-
-    def timerEvent(self):
-        if self.scroller.loaded_pages_count() > self.last_pages_count:
-            self.repaint()
+        self.manga_frame.scrollDown(-event.delta())
+        self.manga_frame.repaint()
 
 
 import sys
---
+++ mangareader/widgets/manga_frame.py
@@ -0,0 +1,49 @@
+"""
+    Author: Hasen "hasenj" il Judy
+    License: GPL v2
+
+    widget that displays the scrollable manga
+"""
+
+# Qt imports
+from PyQt4 import QtGui, QtCore
+
+# Project imports
+from mangareader import fstrip, mscroll
+
+class MangaFrame(QtGui.QWidget):
+    def __init__(self, startdir):
+        QtGui.QWidget.__init__(self, None)
+        self.scroller = mscroll.MangaScroller(startdir)
+        self.last_pages_count = 0 # we use this to know to re-render when new pages are loaded!
+
+        self.timer = QtCore.QTimer()
+        self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.timerEvent)
+        self.timer.start(80) # number is msec
+
+    def scrollDown(self, step=None):
+        self.scroller.scroll_down(step)
+    
+    def scrollUp(self, step=None):
+        self.scroller.scroll_up(step)
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        try: self.last_pages_count = mscroll.paint_scroller(painter, self.scroller)
+        except: print "error in painting"
+        painter.end()
+
+    def timerEvent(self):
+        if self.scroller.loaded_pages_count() > self.last_pages_count:
+            self.repaint()
+
+    def change_manga(self, path):
+        # TODO remember where we were last time we were reading this mange
+        # and restore to the same place
+        self.scroller = mscroll.MangaScroller(path)
+        self.repaint()
+
+    def change_chapter(self, path):
+        self.scroller.change_chapter(path)
+
