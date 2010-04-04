9cc94b3d5859279a5e2ccc409d481047db4c764c
hasen j <hasan.aljudy@gmail.com>
2010-04-04 14:00:16 -0600

    remove 7zipping from build script

--- build.py
+++ build.py
@@ -11,8 +11,6 @@ import os
 
 main = 'manga'
 dest = 'dist_nt'
-version = '0.0.2.x'
-dest_7z = 'mangareader_v%s.7z' % version
 
 def do_exe():
  setup(
@@ -21,11 +19,9 @@ def do_exe():
                 )], 
         options=dict(
             py2exe=dict(
-                optimize=3, 
                 includes=['sip'],
-                compressed=True, 
+                compressed=False, 
                 dist_dir=dest,
-                bundle_files=2
                 )),
         data_files = [ 
             ('art', ['art/icon.png']),
@@ -41,8 +37,5 @@ import py2exe
 os.system("rm -rf " + dest)  # clean previous build
 do_exe()                     # call py2exe
 os.system("git checkout-index -a -f --prefix=" + dest + "/src/") # add source code to build directory (because we're gpl)
-
-os.system("rm " + dest_7z) # remove previous 7z file (if any)
-os.system("7z a " + dest_7z + " " + dest) # create an archive suitable for distribution
 os.system("rm -rf build")    # clean intermediate build files
 
